9736bd6b1f54aff74c94615d16c0b90c68d9ce49
tyler <tylercharawell@gmail.com>
2021-08-02 11:42:54 -0700

    Completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :toys, only: [:index, :create, :update, :destroy]
+  resources :toys, only: [:index, :create, :update]
+  # resources :toys, only: [:index, :create, :update, :destroy]
+
+  delete '/toys:id', to: 'toys#destroy'
 end
